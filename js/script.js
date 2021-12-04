5e7b9ffd89edc792c78cd1fb980ab211ec9ed441
ElenaPadalko <75531045+Ashelinn@users.noreply.github.com>
2021-12-04 22:49:35 +0200

    Delete script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
-
-//Размер холста зависит от ширины экрана
-Resize();
-window.addEventListener("resize", Resize);
